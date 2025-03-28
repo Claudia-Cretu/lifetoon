b7ed6ee5df0fc250143c40b1e66604fee1332114
Cretu Claudia <cretuclaudia373@yahoo.com>
2025-03-28 12:45:47 +0100

    added links footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -11,8 +11,25 @@ export default function Footer({ ...props }) {
             <Heading as="p" className="!font-inter text-[16px] font-normal !text-blue_gray-300">
               © 2025 Lifetoon. All rights reserved.
             </Heading>
+            <div className="flex gap-5">
+              <a
+                href="https://www.linkedin.com/company/lifetoon/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue_gray-300 hover:text-blue_gray-500"
+              >
+                LinkedIn
+              </a>
+              <a
+                href="https://www.instagram.com/lifetoon.me/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue_gray-300 hover:text-blue_gray-500"
+              >
+                Instagram
+              </a>
+            </div>
           </div>
-          
         </div>
       </div>
     </footer>
